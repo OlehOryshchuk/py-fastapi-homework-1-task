a1bccc57c0d0d1837d26588468c6cc9c1c05d628
OlehPro <olehoryshshuk@gmail.com>
2025-01-19 17:44:36 +0100

    feat: Create Pydantic Model Schema for serializing and validating data

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,36 @@
-# Write your code here
+from datetime import datetime
+from decimal import Decimal
+
+from pydantic import (
+    BaseModel,
+    PositiveInt,
+    Field,
+    NonNegativeInt,
+    AnyHttpUrl
+)
+
+
+class MovieDetailResponseSchem(BaseModel):
+    id: PositiveInt
+    name: str
+    date: datetime.date
+    score: float
+    genre: str = Field(..., max_length=255)
+    overview: str
+    crew: str
+    orig_title: str = Field(..., max_length=255)
+    status: str = Field(..., max_length=50)
+    orig_lan: str = Field(..., max_length=50)
+    budget: Decimal = Field(
+        ..., ge=0, le=10**8 - 1, multiple_of=0.01
+    )
+    revenue: float
+    country: Field(..., max_length=3)
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: list[MovieDetailResponseSchem]
+    prev_page: AnyHttpUrl
+    next_page: AnyHttpUrl
+    total_pages: NonNegativeInt
+    total_items: NonNegativeInt
